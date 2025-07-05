a0a367c5477995080822b26b22104fcee1b067c6
Зариф <zarif@MacBook-Pro-Zarif.local>
2025-07-05 15:21:14 +0600

    feat: add Render configuration for frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/real-estate-app/',
+  base: '/',
   plugins: [react()],
+  server: {
+    host: true,
+    port: process.env.PORT || 3000
+  },
+  preview: {
+    host: true,
+    port: process.env.PORT || 3000
+  }
 })
